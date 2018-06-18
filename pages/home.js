884b509d3f955cf6400cd341f3aa81d5ad39ec13
Tom Jane <kodemonki@hotmail.com>
2018-06-18 13:31:58 +0100

    ssr working

---
+++ pages/home.js
@@ -0,0 +1,9 @@
+// home.js
+ 
+export default () => (
+    <div>
+      <p>
+        <a href="https://appdividend.com" target="_blank">Home</a>
+      </p>
+    </div>
+)
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,25 @@
-// This is the Link API
+// index.js
+ 
 import Link from 'next/link'
 
-const Index = () => (
-  <div>
-<Link href="/about">
-  <button>Go to About Page</button>
-</Link>
-    <p>Hello Next.js</p>
-  </div>
-)
+const isClientOrServer = () => {
+  let renderer = '';
+  if(typeof window !== 'undefined' && window.document){
+      renderer = 'Client';
+  }else{
+      renderer = 'Server';
+  }
+  return renderer;
+};
 
-export default Index
+const Index = ({ title = 'Hello from Next.js' }) => (
+  <div>
+    <h1>{title}</h1>
+    <p className="App-intro">
+      Is my application rendered by server or client?
+    </p>
+    <h2><code>{isClientOrServer()}</code></h2>
+  </div>
+);
+
+export default Index;
